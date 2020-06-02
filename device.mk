f1aa47254caa7f5dd4bd9827d9daa7a0f1f9d675
hk1271 <62790149+hk1271@users.noreply.github.com>
2020-06-02 22:18:19 +0530

    Add Prebuilt Gcam Go

--- device.mk
+++ device.mk
@@ -348,3 +348,6 @@ $(call add-product-sanitizer-module-config,mm-qcamera-daemon,never)
 # b/36201281
 $(call add-product-sanitizer-module-config,thermal-engine,never)
 $(call add-product-sanitizer-module-config,qmuxd,never)
+
+# GoogleCamera
+$(call inherit-product, packages/apps/GoogleCamera/gcam.mk)
