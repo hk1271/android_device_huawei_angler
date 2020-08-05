cc2b89bbad439175f5c893fa124e036be1e8d7f8
Hardik <hk912701@gmail.com>
2020-08-05 14:39:41 +0000

    enbale zygote perforking

--- device.mk
+++ device.mk
@@ -354,7 +354,6 @@ $(call add-product-sanitizer-module-config,mm-qcamera-daemon,never)
 $(call add-product-sanitizer-module-config,thermal-engine,never)
 $(call add-product-sanitizer-module-config,qmuxd,never)
 
-<<<<<<< HEAD
 # GoogleCamera
 $(call inherit-product, packages/apps/GoogleCamera/gcam.mk)
 
@@ -362,6 +361,10 @@ $(call inherit-product, packages/apps/GoogleCamera/gcam.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# Zygote Preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
 
 # Remove Apps
 PRODUCT_PACKAGES += \
